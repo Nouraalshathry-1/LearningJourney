f8eeb4605a271d62ffecc081b46d9fdc2b519301
Noura_Alshathry <nouraalshathry>
2025-10-16 15:02:01 +0300

    Initial Commit

---
+++ LearningJourney/LearningJourneyApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningJourneyApp.swift
+//  LearningJourney
+//
+//  Created by Noura Alshathry on 16/10/2025.
+//
+
+import SwiftUI
+
+@main
+struct LearningJourneyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
